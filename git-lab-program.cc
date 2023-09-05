7bd4bee3f99cfb071f676bb96cae6cb9324de5a0
hk484922 <hk484922@ohio.edu>
2023-09-05 17:20:11 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Hunter King
+ *        Date: 9/5/2023
+ * Description: git-lab program, says Hello Git!!
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
